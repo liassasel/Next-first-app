a7d66024ffc10f5a16500acec28d59d7b7659d61
Angel <auseche80@gmail.com>
2024-10-07 14:12:21 -0400

    center

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ServicePage() {
   return (
-    <div>
+    <div className='text-center'>
         <h1>Services Page</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, neque totam eveniet, quisquam voluptate ratione ab illo amet dolor facilis delectus animi. Quos quam et expedita impedit aperiam tempora, iure consectetur accusamus libero! Magni consectetur ea earum aut, nisi unde sunt voluptas numquam possimus quod vitae maiores fugiat consequuntur quas!</p>
     </div>
